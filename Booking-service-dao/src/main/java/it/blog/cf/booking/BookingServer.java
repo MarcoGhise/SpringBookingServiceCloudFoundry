9341b7ccb33d23afcb30ad18088071d9f9b686b8
MarcoGhise <MarcoGhise@users.noreply.github.com>
2018-03-06 16:34:00 +0100

    Update BookingServer.java

--- Booking-service-dao/src/main/java/it/blog/cf/booking/BookingServer.java
+++ Booking-service-dao/src/main/java/it/blog/cf/booking/BookingServer.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableFeignClients
 public class BookingServer {
 
 	public static void main(String[] args) {
